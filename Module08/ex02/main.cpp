126d9f0062795806f4a20fdb2a12071820824a40
Burak <104313678+burak-yldrm@users.noreply.github.com>
2023-11-04 12:27:33 +0000

    test için std::list yapısı eklendi

--- Module08/ex02/main.cpp
+++ Module08/ex02/main.cpp
@@ -1,13 +1,15 @@
 #include "MutantStack.hpp"
+#include <list>
 
 int main()
 {
 	MutantStack<int> mstack;
+
 	mstack.push(5);
 	mstack.push(17);
-	// std::cout << mstack.top() << std::endl;
+	std::cout << mstack.top() << std::endl;
 	mstack.pop();
-	// std::cout << mstack.size() << std::endl;
+	std::cout << mstack.size() << std::endl;
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
@@ -29,5 +31,37 @@ int main()
 	std::stack<int> s(mstack);
 
 	std::cout << s.size() << std::endl;
+
+
+	std::cout << "\n\nLIST EXAMPLE------------------\n\n";
+
+	std::list<int> tempList;
+	tempList.push_back(5);
+	tempList.push_back(17);
+	std::cout << tempList.back() << std::endl;
+	tempList.pop_back();
+	std::cout << tempList.size() << std::endl;
+	tempList.push_back(3);
+	tempList.push_back(5);
+	tempList.push_back(737);
+	tempList.push_back(0);
+
+	std::list<int> tempList2;
+	tempList2 = tempList;
+
+	std::list<int>::iterator lstIt = tempList.begin();
+	std::list<int>::iterator lstIt2 = tempList.end();
+
+	++lstIt;
+	--lstIt;
+
+	while (lstIt != lstIt2)
+	{
+		std::cout << *lstIt << std::endl;
+		++lstIt;
+	}
+	std::list<int> ss(tempList);
+
+	std::cout << ss.size() << std::endl;
 	return 0;
 }
